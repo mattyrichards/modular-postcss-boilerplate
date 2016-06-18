a3d2b3d8308560a935f3f2194998cdf37b8fb73a
mattyrichards <matt@kreative.co.uk>
2016-06-18 14:35:50 +0100

    Added more building blocks

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ To be continued...
 
 HTML5 Boilerplate https://html5boilerplate.com/
 Normalize.css https://necolas.github.io/normalize.css/
+
+PureCSS grid
+
+PostCSS
+List all postcss plugins/modules (correct terminology) and small description.
+
+Responsive suffixes
+http://csswizardry.com/2015/08/bemit-taking-the-bem-naming-convention-a-step-further/
+AND
+postcss-custom-media
--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,65 @@
-var paths = require('./paths');
-var gulp = require('gulp');
-var postcss = require('gulp-postcss');
-var stylelint = require('stylelint');
-var reporter = require('postcss-reporter');
-var autoprefixer = require('autoprefixer');
-var atImport = require('postcss-import');
-var cssnano = require('cssnano');
-var rename = require('gulp-rename');
-var simpleVars = require('postcss-simple-vars');
-var nested = require('postcss-nested');
+var paths = require('./paths'),
+    gulp = require('gulp'),
+    postcss = require('gulp-postcss'),
+    stylelint = require('stylelint'),
+    reporter = require('postcss-reporter'),
+    autoprefixer = require('autoprefixer'),
+    atImport = require('postcss-import'),
+    cssnano = require('cssnano'),
+    rename = require('gulp-rename'),
+    nested = require('postcss-nested'),
+    mixins = require('postcss-mixins'),
+    discardComments = require('postcss-discard-comments'),
+    calc = require('postcss-calc'),
+    customMedia = require('postcss-custom-media'),
+    simpleVars = require('postcss-simple-vars');
 
 gulp.task('stylelint', function () {
-  return gulp.src('style.less')
+  return gulp.src(paths.sourceCSS + '/**/*.css')
     .pipe(postcss(
       [
         stylelint(),
         reporter({ clearMessages: true })
-      ],
-      {
-        syntax: less
-      }
+      ]
     ));
 });
 
-// TODO: create "build" version with cssnano as processor for minification. Don't bother while in dev so we can check output easily
-gulp.task('css', function () {
+// Building CSS for development use
+gulp.task('build-css-dev', function () {
   var processors = [
     atImport,
-    simpleVars,
     nested,
-    autoprefixer({browsers: ['last 2 versions']})
+    mixins,
+    customMedia,
+    simpleVars,
+    calc({mediaQueries: true})
   ];
   return gulp.src(paths.sourceCSS + '/root.css')
     .pipe(postcss(processors))
     .pipe(rename('main.css'))
     .pipe(gulp.dest(paths.buildCSS));
+});
+
+// Building CSS for production use.
+// Note inclusion of cssnano (minification/optimisation), Autoprefixer & discardComments
+gulp.task('build-css-prod', function () {
+  var processors = [
+    atImport,
+    nested,
+    mixins,
+    simpleVars,
+    customMedia,
+    calc({mediaQueries: true}),
+    discardComments,
+    autoprefixer({browsers: ['last 2 versions']}),
+    cssnano
+  ];
+  return gulp.src(paths.sourceCSS + '/root.css')
+    .pipe(postcss(processors))
+    .pipe(rename('main.css'))
+    .pipe(gulp.dest(paths.buildCSS));
+});
+
+gulp.task('default', ['stylelint', 'build-css-dev'], function() {
+  gulp.watch(paths.sourceCSS + '/**/*.css', ['stylelint', 'build-css-dev']);
 });
