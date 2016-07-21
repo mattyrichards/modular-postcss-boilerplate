7b27a287a86cbc48c9e5f9516135e2f34d1b000b
Matt Richards <matt@kreative.co.uk>
2016-07-21 21:58:40 +0100

    Removed iconpaths from paths config

--- paths.js
+++ paths.js
@@ -3,9 +3,7 @@ var path = require('path');
 module.exports = {
 	source: path.join(__dirname, 'source'),
   sourceCSS: path.join(__dirname, 'source/css'),
-	sourceIcons: path.join(__dirname, 'node_modules/open-iconic/svg'),
 	build: path.join(__dirname, 'build'),
   buildCSS: path.join(__dirname, 'build/css'),
-	buildIcons: path.join(__dirname, 'build/images/icons'),
 	buildStyleGuide: path.join(__dirname, 'build/styleguide')
 };
